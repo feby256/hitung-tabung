2bbdfe96686e01929d681c48a27224de56fcfa1d
feby256 <febi30289@gmail.com>
2024-12-03 21:36:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,9 @@ t = st.number_input("masukan tinggi(cm): ",0)
 
 if st.button("hitung volume", type="primary"):
   loding = st.progress(0)
-  for percent_complete in range(100):
+  for i in range(100):
     time.sleep(0.01)
     loading.progress(i+1)
+    
   v = math.pi*(r**2)*t
   st.success(f'volume tabung adalah {v:.2f}')
